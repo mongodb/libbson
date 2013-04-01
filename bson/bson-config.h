fad4d22c0ffa27635c85bf2ebddde6185ab31825
Christian Hergert <christian.hergert@10gen.com>
2013-04-01 15:16:07 -0700

    build: check host endianness at compile time.

--- bson/bson-config.h
+++ bson/bson-config.h
@@ -2,10 +2,6 @@
  * This is a temporary config.h until the build system generates it.
  */
 
-
-#define HAVE_STDINT 1
-#define BSON_BYTE_ORDER 1234
-
 #if defined(_MSC_VER) || defined(_WIN32) || defined(_WIN64)
 #define HAVE_WINDOWS
 #endif
