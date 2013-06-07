ec17367415d45c44e998acca2023060a8c3f987b
Christian Hergert <christian.hergert@10gen.com>
2013-06-07 14:26:33 -0700

    tests: check return value of bson_append_*().

--- tests/test-bson-json.c
+++ tests/test-bson-json.c
@@ -18,32 +18,32 @@ test_bson_as_json (void)
    bson_oid_init_from_string(&oid, "123412341234abcdabcdabcd");
 
    b = bson_new();
-   bson_append_utf8(b, "utf8", -1, "bar", -1);
-   bson_append_int32(b, "int32", -1, 1234);
-   bson_append_int64(b, "int64", -1, 4321);
-   bson_append_double(b, "double", -1, 123.4);
-   bson_append_undefined(b, "undefined", -1);
-   bson_append_null(b, "null", -1);
-   bson_append_oid(b, "oid", -1, &oid);
-   bson_append_bool(b, "true", -1, TRUE);
-   bson_append_bool(b, "false", -1, FALSE);
-   bson_append_time_t(b, "date", -1, time(NULL));
-   bson_append_timestamp(b, "timestamp", -1, time(NULL), 1234);
-   bson_append_regex(b, "regex", -1, "^abcd", "xi");
-   bson_append_dbpointer(b, "dbpointer", -1, "mycollection", &oid);
-   bson_append_minkey(b, "minkey", -1);
-   bson_append_maxkey(b, "maxkey", -1);
-   bson_append_symbol(b, "symbol", -1, "var a = {};", -1);
+   assert(bson_append_utf8(b, "utf8", -1, "bar", -1));
+   assert(bson_append_int32(b, "int32", -1, 1234));
+   assert(bson_append_int64(b, "int64", -1, 4321));
+   assert(bson_append_double(b, "double", -1, 123.4));
+   assert(bson_append_undefined(b, "undefined", -1));
+   assert(bson_append_null(b, "null", -1));
+   assert(bson_append_oid(b, "oid", -1, &oid));
+   assert(bson_append_bool(b, "true", -1, TRUE));
+   assert(bson_append_bool(b, "false", -1, FALSE));
+   assert(bson_append_time_t(b, "date", -1, time(NULL)));
+   assert(bson_append_timestamp(b, "timestamp", -1, time(NULL), 1234));
+   assert(bson_append_regex(b, "regex", -1, "^abcd", "xi"));
+   assert(bson_append_dbpointer(b, "dbpointer", -1, "mycollection", &oid));
+   assert(bson_append_minkey(b, "minkey", -1));
+   assert(bson_append_maxkey(b, "maxkey", -1));
+   assert(bson_append_symbol(b, "symbol", -1, "var a = {};", -1));
 
    b2 = bson_new();
-   bson_append_int32(b2, "0", -1, 60);
-   bson_append_document(b, "document", -1, b2);
-   bson_append_array(b, "array", -1, b2);
+   assert(bson_append_int32(b2, "0", -1, 60));
+   assert(bson_append_document(b, "document", -1, b2));
+   assert(bson_append_array(b, "array", -1, b2));
 
    {
       const bson_uint8_t binary[] = { 0, 1, 2, 3, 4 };
-      bson_append_binary(b, "binary", -1, BSON_SUBTYPE_BINARY,
-                         binary, sizeof binary);
+      assert(bson_append_binary(b, "binary", -1, BSON_SUBTYPE_BINARY,
+                                binary, sizeof binary));
    }
 
    for (i = 0; i < 1000; i++) {
@@ -64,7 +64,7 @@ test_bson_as_json_string (void)
    char *str;
 
    b = bson_new();
-   bson_append_utf8(b, "foo", -1, "bar", -1);
+   assert(bson_append_utf8(b, "foo", -1, "bar", -1));
    str = bson_as_json(b, &len);
    assert(len == 17);
    assert(!strcmp("{ \"foo\" : \"bar\" }", str));
@@ -81,7 +81,7 @@ test_bson_as_json_int32 (void)
    char *str;
 
    b = bson_new();
-   bson_append_int32(b, "foo", -1, 1234);
+   assert(bson_append_int32(b, "foo", -1, 1234));
    str = bson_as_json(b, &len);
    assert(len == 16);
    assert(!strcmp("{ \"foo\" : 1234 }", str));
@@ -98,7 +98,7 @@ test_bson_as_json_int64 (void)
    char *str;
 
    b = bson_new();
-   bson_append_int64(b, "foo", -1, 341234123412341234UL);
+   assert(bson_append_int64(b, "foo", -1, 341234123412341234UL));
    str = bson_as_json(b, &len);
    assert(len == 30);
    assert(!strcmp("{ \"foo\" : 341234123412341234 }", str));
@@ -115,7 +115,7 @@ test_bson_as_json_double (void)
    char *str;
 
    b = bson_new();
-   bson_append_double(b, "foo", -1, 123.456);
+   assert(bson_append_double(b, "foo", -1, 123.456));
    str = bson_as_json(b, &len);
    assert(len == 22);
    assert(!strcmp("{ \"foo\" : 123.456000 }", str));
@@ -132,7 +132,7 @@ test_bson_as_json_utf8 (void)
    char *str;
 
    b = bson_new();
-   bson_append_utf8(b, "€€€€€", -1, "€€€€€", -1);
+   assert(bson_append_utf8(b, "€€€€€", -1, "€€€€€", -1));
    str = bson_as_json(b, &len);
    assert(!strcmp(str, "{ \"€€€€€\" : \"€€€€€\" }"));
    bson_free(str);
