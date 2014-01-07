7e5a2eacb8bb177d701b1fa2a1ed41e94087102a
Christian Hergert <christian.hergert@10gen.com>
2014-01-07 09:52:03 -0500

    bson: add convenience macros for appending using string literals.
    strlen(literal) compiles out on GCC and Clang, so by using these macros
    you save some typing and cleanliness and get a bit of a speed improvement
    over passing -1 for length.

--- tests/test-bson.c
+++ tests/test-bson.c
@@ -1178,6 +1178,49 @@ test_bson_reinit (void)
 
 
 static void
+test_bson_macros (void)
+{
+   const bson_uint8_t data [] = { 1, 2, 3, 4 };
+   bson_t b = BSON_INITIALIZER;
+   bson_t ar = BSON_INITIALIZER;
+   bson_oid_t oid;
+   struct timeval tv;
+   time_t t;
+
+   t = time (NULL);
+   tv.tv_sec = t;
+   tv.tv_usec = 0;
+
+   bson_oid_init (&oid, NULL);
+
+   BSON_APPEND_ARRAY (&b, "0", &ar);
+   BSON_APPEND_BINARY (&b, "1", 0, data, sizeof data);
+   BSON_APPEND_BOOL (&b, "2", TRUE);
+   BSON_APPEND_CODE (&b, "3", "function(){}");
+   BSON_APPEND_CODE_WITH_SCOPE (&b, "4", "function(){}", &ar);
+   BSON_APPEND_DOUBLE (&b, "6", 123.45);
+   BSON_APPEND_DOCUMENT (&b, "7", &ar);
+   BSON_APPEND_INT32 (&b, "8", 123);
+   BSON_APPEND_INT64 (&b, "9", 456);
+   BSON_APPEND_MINKEY (&b, "10");
+   BSON_APPEND_MAXKEY (&b, "11");
+   BSON_APPEND_NULL (&b, "12");
+   BSON_APPEND_OID (&b, "13", &oid);
+   BSON_APPEND_REGEX (&b, "14", "^abc", "i");
+   BSON_APPEND_UTF8 (&b, "15", "utf8");
+   BSON_APPEND_SYMBOL (&b, "16", "symbol");
+   BSON_APPEND_TIME_T (&b, "17", t);
+   BSON_APPEND_TIMEVAL (&b, "18", &tv);
+   BSON_APPEND_DATE_TIME (&b, "19", 123);
+   BSON_APPEND_TIMESTAMP (&b, "20", 123, 0);
+   BSON_APPEND_UNDEFINED (&b, "21");
+
+   bson_destroy (&b);
+   bson_destroy (&ar);
+}
+
+
+static void
 init_rand (void)
 {
    unsigned seed;
@@ -1246,6 +1289,7 @@ main (int   argc,
    run_test("/bson/initializer", test_bson_initializer);
    run_test("/bson/concat", test_bson_concat);
    run_test("/bson/reinit", test_bson_reinit);
+   run_test("/bson/macros", test_bson_macros);
 
    return 0;
 }
