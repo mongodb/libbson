7e24474ce7d9d1d6ab7c9153708ceab87f60094b
Christian Hergert <christian.hergert@mongodb.com>
2014-02-20 15:07:15 -0800

    context: docs and whitespace.

--- bson/bson-context.c
+++ bson/bson-context.c
@@ -40,6 +40,9 @@
 #endif
 
 
+/*
+ * Globals.
+ */
 static bson_context_t *gContextDefault;
 
 
@@ -52,14 +55,34 @@ gettid (void)
 #endif
 
 
+/*
+ *--------------------------------------------------------------------------
+ *
+ * _bson_context_get_oid_host --
+ *
+ *       Retrieves the first three bytes of MD5(hostname) and assigns them
+ *       to the host portion of oid.
+ *
+ * Returns:
+ *       None.
+ *
+ * Side effects:
+ *       @oid is modified.
+ *
+ *--------------------------------------------------------------------------
+ */
+
 static void
-_bson_context_get_oid_host (bson_context_t *context,
-                            bson_oid_t     *oid)
+_bson_context_get_oid_host (bson_context_t *context,  /* IN */
+                            bson_oid_t     *oid)      /* OUT */
 {
    uint8_t *bytes = (uint8_t *)oid;
    uint8_t digest[16];
    bson_md5_t md5;
    char hostname[HOST_NAME_MAX];
+
+   BSON_ASSERT (context);
+   BSON_ASSERT (oid);
 
    gethostname (hostname, sizeof hostname);
    hostname[HOST_NAME_MAX - 1] = '\0';
@@ -74,10 +97,29 @@ _bson_context_get_oid_host (bson_context_t *context,
 }
 
 
+/*
+ *--------------------------------------------------------------------------
+ *
+ * _bson_context_get_oid_host_cached --
+ *
+ *       Fetch the cached copy of the MD5(hostname).
+ *
+ * Returns:
+ *       None.
+ *
+ * Side effects:
+ *       @oid is modified.
+ *
+ *--------------------------------------------------------------------------
+ */
+
 static void
-_bson_context_get_oid_host_cached (bson_context_t *context,
-                                   bson_oid_t     *oid)
+_bson_context_get_oid_host_cached (bson_context_t *context, /* IN */
+                                   bson_oid_t     *oid)     /* OUT */
 {
+   BSON_ASSERT (context);
+   BSON_ASSERT (oid);
+
    oid->bytes[4] = context->md5[0];
    oid->bytes[5] = context->md5[1];
    oid->bytes[6] = context->md5[2];
@@ -101,12 +143,33 @@ _bson_getpid (void)
 }
 
 
+/*
+ *--------------------------------------------------------------------------
+ *
+ * _bson_context_get_oid_pid --
+ *
+ *       Initialize the pid field of @oid.
+ *
+ *       The pid field is 2 bytes, big-endian for memcmp().
+ *
+ * Returns:
+ *       None.
+ *
+ * Side effects:
+ *       @oid is modified.
+ *
+ *--------------------------------------------------------------------------
+ */
+
 static void
-_bson_context_get_oid_pid (bson_context_t *context,
-                           bson_oid_t     *oid)
+_bson_context_get_oid_pid (bson_context_t *context, /* IN */
+                           bson_oid_t     *oid)     /* OUT */
 {
    uint16_t pid = _bson_getpid ();
    uint8_t *bytes = (uint8_t *)&pid;
+
+   BSON_ASSERT (context);
+   BSON_ASSERT (oid);
 
    pid = BSON_UINT16_TO_BE (pid);
 
@@ -115,18 +178,52 @@ _bson_context_get_oid_pid (bson_context_t *context,
 }
 
 
+/*
+ *--------------------------------------------------------------------------
+ *
+ * _bson_context_get_oid_pid_cached --
+ *
+ *       Fetch the cached copy of the current pid.
+ *       This helps avoid multiple calls to getpid() which is slower
+ *       on some systems.
+ *
+ * Returns:
+ *       None.
+ *
+ * Side effects:
+ *       @oid is modified.
+ *
+ *--------------------------------------------------------------------------
+ */
+
 static void
-_bson_context_get_oid_pid_cached (bson_context_t *context,
-                                  bson_oid_t     *oid)
+_bson_context_get_oid_pid_cached (bson_context_t *context, /* IN */
+                                  bson_oid_t     *oid)     /* OUT */
 {
    oid->bytes[7] = context->pidbe[0];
    oid->bytes[8] = context->pidbe[1];
 }
 
 
+/*
+ *--------------------------------------------------------------------------
+ *
+ * _bson_context_get_oid_seq32 --
+ *
+ *       32-bit sequence generator, non-thread-safe version.
+ *
+ * Returns:
+ *       None.
+ *
+ * Side effects:
+ *       @oid is modified.
+ *
+ *--------------------------------------------------------------------------
+ */
+
 static void
-_bson_context_get_oid_seq32 (bson_context_t *context,
-                             bson_oid_t     *oid)
+_bson_context_get_oid_seq32 (bson_context_t *context, /* IN */
+                             bson_oid_t     *oid)     /* OUT */
 {
    uint32_t seq = context->seq32++;
 
@@ -135,9 +232,25 @@ _bson_context_get_oid_seq32 (bson_context_t *context,
 }
 
 
+/*
+ *--------------------------------------------------------------------------
+ *
+ * _bson_context_get_oid_seq32_threadsafe --
+ *
+ *       Thread-safe version of 32-bit sequence generator.
+ *
+ * Returns:
+ *       None.
+ *
+ * Side effects:
+ *       @oid is modified.
+ *
+ *--------------------------------------------------------------------------
+ */
+
 static void
-_bson_context_get_oid_seq32_threadsafe (bson_context_t *context,
-                                        bson_oid_t     *oid)
+_bson_context_get_oid_seq32_threadsafe (bson_context_t *context, /* IN */
+                                        bson_oid_t     *oid)     /* OUT */
 {
 #if defined WITH_OID32_PT
    uint32_t seq;
@@ -153,20 +266,55 @@ _bson_context_get_oid_seq32_threadsafe (bson_context_t *context,
 }
 
 
-static void
-_bson_context_get_oid_seq64 (bson_context_t *context,
-                             bson_oid_t     *oid)
-{
-   uint64_t seq = context->seq64++;
+/*
+ *--------------------------------------------------------------------------
+ *
+ * _bson_context_get_oid_seq64 --
+ *
+ *       64-bit oid sequence generator, non-thread-safe version.
+ *
+ * Returns:
+ *       None.
+ *
+ * Side effects:
+ *       @oid is modified.
+ *
+ *--------------------------------------------------------------------------
+ */
 
-   seq = BSON_UINT64_TO_BE (seq);
+static void
+_bson_context_get_oid_seq64 (bson_context_t *context, /* IN */
+                             bson_oid_t     *oid)     /* OUT */
+{
+   uint64_t seq;
+
+   BSON_ASSERT (context);
+   BSON_ASSERT (oid);
+
+   seq = BSON_UINT64_TO_BE (context->seq64++);
    memcpy (&oid->bytes[4], &seq, 8);
 }
 
 
+/*
+ *--------------------------------------------------------------------------
+ *
+ * _bson_context_get_oid_seq64_threadsafe --
+ *
+ *       Thread-safe 64-bit sequence generator.
+ *
+ * Returns:
+ *       None.
+ *
+ * Side effects:
+ *       @oid is modified.
+ *
+ *--------------------------------------------------------------------------
+ */
+
 static void
-_bson_context_get_oid_seq64_threadsafe (bson_context_t *context,
-                                        bson_oid_t     *oid)
+_bson_context_get_oid_seq64_threadsafe (bson_context_t *context, /* IN */
+                                        bson_oid_t     *oid)     /* OUT */
 {
 #if defined WITH_OID64_PT
    uint64_t seq;
@@ -188,29 +336,47 @@ _bson_context_get_oid_seq64_threadsafe (bson_context_t *context,
  * bson_context_new:
  * @flags: A #bson_context_flags_t.
  *
- * Initializes a new context with the flags specified.
- *
- * In most cases, you want to call this with @flags set to BSON_CONTEXT_NONE.
- *
- * If you are running on Linux, %BSON_CONTEXT_USE_TASK_ID can result in a
- * healthy speedup for multi-threaded scenarios.
- *
- * If you absolutely must have a single context for your application and use
- * more than one thread, then %BSON_CONTEXT_THREAD_SAFE should be bitwise-or'd
- * with your flags. This requires synchronization between threads.
- *
- * If you expect your hostname to change often, you may consider specifying
- * %BSON_CONTEXT_DISABLE_HOST_CACHE so that gethostname() is called for every
- * OID generated. This is much slower.
- *
- * If you expect your pid to change without notice, such as from an unexpected
- * call to fork(), then specify %BSON_CONTEXT_DISABLE_PID_CACHE.
- *
  * Returns: (transfer full): A newly allocated bson_context_t that should be
  *   freed with bson_context_destroy().
  */
+/*
+ *--------------------------------------------------------------------------
+ *
+ * bson_context_new --
+ *
+ *       Initializes a new context with the flags specified.
+ *
+ *       In most cases, you want to call this with @flags set to
+ *       BSON_CONTEXT_NONE.
+ *
+ *       If you are running on Linux, %BSON_CONTEXT_USE_TASK_ID can result
+ *       in a healthy speedup for multi-threaded scenarios.
+ *
+ *       If you absolutely must have a single context for your application
+ *       and use more than one thread, then %BSON_CONTEXT_THREAD_SAFE should
+ *       be bitwise-or'd with your flags. This requires synchronization
+ *       between threads.
+ *
+ *       If you expect your hostname to change often, you may consider
+ *       specifying %BSON_CONTEXT_DISABLE_HOST_CACHE so that gethostname()
+ *       is called for every OID generated. This is much slower.
+ *
+ *       If you expect your pid to change without notice, such as from an
+ *       unexpected call to fork(), then specify
+ *       %BSON_CONTEXT_DISABLE_PID_CACHE.
+ *
+ * Returns:
+ *       A newly allocated bson_context_t that should be freed with
+ *       bson_context_destroy().
+ *
+ * Side effects:
+ *       None.
+ *
+ *--------------------------------------------------------------------------
+ */
+
 bson_context_t *
-bson_context_new (bson_context_flags_t flags)
+bson_context_new (bson_context_flags_t flags) /* IN */
 {
    bson_context_t *context;
    struct timeval tv;
@@ -291,15 +457,25 @@ bson_context_new (bson_context_flags_t flags)
 }
 
 
-/**
- * bson_context_destroy:
- * @context: A #bson_context_t.
+/*
+ *--------------------------------------------------------------------------
  *
- * Cleans up a bson_context_t and releases any associated resources. This
- * should be called when you are done using @context.
+ * bson_context_destroy --
+ *
+ *       Cleans up a bson_context_t and releases any associated resources.
+ *       This should be called when you are done using @context.
+ *
+ * Returns:
+ *       None.
+ *
+ * Side effects:
+ *       None.
+ *
+ *--------------------------------------------------------------------------
  */
+
 void
-bson_context_destroy (bson_context_t *context)
+bson_context_destroy (bson_context_t *context)  /* IN */
 {
 #if defined WITH_OID32_PT
    bson_mutex_destroy (&context->_m32);
@@ -311,6 +487,22 @@ bson_context_destroy (bson_context_t *context)
    bson_free (context);
 }
 
+
+/*
+ *--------------------------------------------------------------------------
+ *
+ * _bson_context_destroy_default --
+ *
+ *       Cleanup the default context on exit.
+ *
+ * Returns:
+ *       None.
+ *
+ * Side effects:
+ *       None.
+ *
+ *--------------------------------------------------------------------------
+ */
 
 static void
 _bson_context_destroy_default (void)
@@ -332,16 +524,25 @@ BSON_ONCE_FUN(_bson_context_init_default)
 }
 
 
-/**
- * bson_context_get_default:
+/*
+ *--------------------------------------------------------------------------
  *
- * Fetches the default, thread-safe implementation of #bson_context_t.
- * If you need faster generation, it is recommended you create your
- * own #bson_context_t with bson_context_new().
+ * bson_context_get_default --
  *
- * Returns: A shared instance to the default #bson_context_t. This should not
- *    be modified or freed.
+ *       Fetches the default, thread-safe implementation of #bson_context_t.
+ *       If you need faster generation, it is recommended you create your
+ *       own #bson_context_t with bson_context_new().
+ *
+ * Returns:
+ *       A shared instance to the default #bson_context_t. This should not
+ *       be modified or freed.
+ *
+ * Side effects:
+ *       None.
+ *
+ *--------------------------------------------------------------------------
  */
+
 bson_context_t *
 bson_context_get_default (void)
 {
