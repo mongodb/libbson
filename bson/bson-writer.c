0c85658e89b708118d75ba000b5e0040c481ba70
Christian Hergert <christian.hergert@mongodb.com>
2014-02-27 15:55:07 -0800

    writer: whitespace, docs.

--- bson/bson-writer.c
+++ bson/bson-writer.c
@@ -15,41 +15,57 @@
  */
 
 
+#ifdef HAVE_CONFIG_H
+# include <config.h>
+#endif
+
 #include "bson-private.h"
 #include "bson-writer.h"
 
 
 struct _bson_writer_t
 {
-   bool       ready;
-   uint8_t    **buf;
-   size_t           *buflen;
-   size_t            offset;
-   bson_realloc_func realloc_func;
-   bson_t            b;
+   bool                ready;
+   uint8_t           **buf;
+   size_t             *buflen;
+   size_t              offset;
+   bson_realloc_func   realloc_func;
+   bson_t              b;
 };
 
 
-/**
- * bson_writer_new:
- * @buf: (inout): A pointer to a target buffer.
- * @buflen: (inout): A pointer to the buffer length.
- * @offset: The offset in the target buffer to start from.
- * @realloc_func: A realloc() style function or NULL.
+/*
+ *--------------------------------------------------------------------------
  *
- * Creates a new instance of bson_writer_t using the buffer, length, offset,
- * and realloc() function supplied.
+ * bson_writer_new --
  *
- * The caller is expected to clean up the structure when finished using
- * bson_writer_destroy().
+ *       Creates a new instance of bson_writer_t using the buffer, length,
+ *       offset, and realloc() function supplied.
  *
- * Returns: A newly allocated bson_writer_t.
+ *       The caller is expected to clean up the structure when finished
+ *       using bson_writer_destroy().
+ *
+ * Parameters:
+ *       @buf: (inout): A pointer to a target buffer.
+ *       @buflen: (inout): A pointer to the buffer length.
+ *       @offset: The offset in the target buffer to start from.
+ *       @realloc_func: A realloc() style function or NULL.
+ *
+ * Returns:
+ *       A newly allocated bson_writer_t that should be freed with
+ *       bson_writer_destroy().
+ *
+ * Side effects:
+ *       None.
+ *
+ *--------------------------------------------------------------------------
  */
+
 bson_writer_t *
-bson_writer_new (uint8_t    **buf,
-                 size_t           *buflen,
-                 size_t            offset,
-                 bson_realloc_func realloc_func)
+bson_writer_new (uint8_t           **buf,          /* IN */
+                 size_t             *buflen,       /* IN */
+                 size_t              offset,       /* IN */
+                 bson_realloc_func   realloc_func) /* IN */
 {
    bson_writer_t *writer;
 
@@ -64,54 +80,86 @@ bson_writer_new (uint8_t    **buf,
 }
 
 
-/**
- * bson_writer_destroy:
- * @writer: A bson_writer_t
+/*
+ *--------------------------------------------------------------------------
  *
- * Cleanup after @writer and release any allocated memory. Note that the buffer
- * supplied to bson_writer_new() is NOT freed from this method.  The caller is
- * responsible for that.
+ * bson_writer_destroy --
+ *
+ *       Cleanup after @writer and release any allocated memory. Note that
+ *       the buffer supplied to bson_writer_new() is NOT freed from this
+ *       method.  The caller is responsible for that.
+ *
+ * Returns:
+ *       None.
+ *
+ * Side effects:
+ *       None.
+ *
+ *--------------------------------------------------------------------------
  */
+
 void
-bson_writer_destroy (bson_writer_t *writer)
+bson_writer_destroy (bson_writer_t *writer) /* IN */
 {
    bson_free (writer);
 }
 
 
-/**
- * bson_writer_get_length:
- * @writer: A bson_writer_t.
+/*
+ *--------------------------------------------------------------------------
  *
- * Fetches the current length of the content written by the buffer (including
- * the initial offset). This includes a partly written document currently being
- * written.
+ * bson_writer_get_length --
  *
- * This is useful if you want to check to see if you've passed a given memory
- * boundry that cannot be sent in a packet. See bson_writer_rollback() to abort
- * the current document being written.
+ *       Fetches the current length of the content written by the buffer
+ *       (including the initial offset). This includes a partly written
+ *       document currently being written.
  *
- * Returns: The number of bytes written plus initial offset.
+ *       This is useful if you want to check to see if you've passed a given
+ *       memory boundry that cannot be sent in a packet. See
+ *       bson_writer_rollback() to abort the current document being written.
+ *
+ * Returns:
+ *       The number of bytes written plus initial offset.
+ *
+ * Side effects:
+ *       None.
+ *
+ *--------------------------------------------------------------------------
  */
+
 size_t
-bson_writer_get_length (bson_writer_t *writer)
+bson_writer_get_length (bson_writer_t *writer) /* IN */
 {
    return writer->offset + writer->b.len;
 }
 
 
-/**
- * bson_writer_begin:
- * @writer: A bson_writer_t.
- * @bson: (out): A location for a bson_t*.
+/*
+ *--------------------------------------------------------------------------
  *
- * Begins writing a new document. The caller may use the bson structure to
- * write out a new BSON document. When completed, the caller must call either
- * bson_writer_end() or bson_writer_rollback().
+ * bson_writer_begin --
+ *
+ *       Begins writing a new document. The caller may use the bson
+ *       structure to write out a new BSON document. When completed, the
+ *       caller must call either bson_writer_end() or
+ *       bson_writer_rollback().
+ *
+ * Parameters:
+ *       @writer: A bson_writer_t.
+ *       @bson: (out): A location for a bson_t*.
+ *
+ * Returns:
+ *       true if the underlying realloc was successful; otherwise false.
+ *
+ * Side effects:
+ *       @bson is initialized if true is returned.
+ *
+ *--------------------------------------------------------------------------
  */
+
 bool
-bson_writer_begin (bson_writer_t *writer,
-                   bson_t       **bson)
+bson_writer_begin (bson_writer_t  *writer, /* IN */
+                   bson_t        **bson)   /* OUT */
 {
    bson_impl_alloc_t *b;
    bool grown = false;
@@ -163,14 +211,24 @@ bson_writer_begin (bson_writer_t *writer,
 }
 
 
-/**
- * bson_writer_end:
- * @writer: A bson_t.
+/*
+ *--------------------------------------------------------------------------
  *
- * Complete writing of a bson_writer_t to the buffer supplied.
+ * bson_writer_end --
+ *
+ *       Complete writing of a bson_writer_t to the buffer supplied.
+ *
+ * Returns:
+ *       None.
+ *
+ * Side effects:
+ *       None.
+ *
+ *--------------------------------------------------------------------------
  */
+
 void
-bson_writer_end (bson_writer_t *writer)
+bson_writer_end (bson_writer_t *writer) /* IN */
 {
    bson_return_if_fail (writer);
    bson_return_if_fail (!writer->ready);
@@ -181,16 +239,27 @@ bson_writer_end (bson_writer_t *writer)
 }
 
 
-/**
- * bson_writer_rollback:
- * @writer: A bson_writer_t.
+/*
+ *--------------------------------------------------------------------------
  *
- * Abort the appending of the current bson_t to the memory region managed by
- * @writer.  This is useful if you detected that you went past a particular
- * memory limit.  For example, MongoDB has 48MB message limits.
+ * bson_writer_rollback --
+ *
+ *       Abort the appending of the current bson_t to the memory region
+ *       managed by @writer.  This is useful if you detected that you went
+ *       past a particular memory limit.  For example, MongoDB has 48MB
+ *       message limits.
+ *
+ * Returns:
+ *       None.
+ *
+ * Side effects:
+ *       None.
+ *
+ *--------------------------------------------------------------------------
  */
+
 void
-bson_writer_rollback (bson_writer_t *writer)
+bson_writer_rollback (bson_writer_t *writer) /* IN */
 {
    bson_return_if_fail (writer);
 
