e97162fc32464bfff7165b5d3038c6045402acff
Christian Hergert <christian.hergert@mongodb.com>
2014-03-01 01:51:32 -0800

    examples: cleanup bson-validate example.

--- examples/bson-validate.c
+++ examples/bson-validate.c
@@ -16,62 +16,21 @@
 
 
 /*
- * This program will validate each BSON document contained in the provided
- * files.
+ * This program will validate each BSON document contained in the files provide
+ * as arguments to the program.  Each document from each file is read in
+ * sequence until a bad BSON document is found or there are no more documents
+ * to read.
+ *
+ * Try running it with:
+ *
+ * ./bson-validate tests/binary/overflow2.bson
+ * ./bson-validate tests/binary/trailingnull.bson
  */
 
 
 #include <bson.h>
-#include <errno.h>
-#include <fcntl.h>
 #include <stdio.h>
-
-#ifdef BSON_OS_WIN32
-#include <share.h>
-int
-bson_open (const char *filename,
-           int         flags)
-{
-   int fd;
-   errno_t err;
-
-   err = _sopen_s (&fd, filename, flags | _O_BINARY, _SH_DENYNO,
-                   _S_IREAD | _S_IWRITE);
-
-   if (err) {
-      errno = err;
-      return -1;
-   }
-
-   return fd;
-}
-
-ssize_t
-bson_read (int    fd,
-           void  *buf,
-           size_t count)
-{
-   return (ssize_t)_read (fd, buf, (int)count);
-}
-
-#define bson_close _close
-#else
-#define bson_open open
-#define bson_read read
-#define bson_close close
-#endif
-
-static ssize_t
-_read_cb(void * handle, void * buf, size_t len)
-{
-   return bson_read(*(int *)handle, buf, len);
-}
-
-static void
-_destroy_cb(void * handle)
-{
-   bson_close(*(int *)handle);
-}
+#include <stdlib.h>
 
 
 int
@@ -80,17 +39,17 @@ main (int   argc,
 {
    bson_reader_t *reader;
    const bson_t *b;
+   bson_error_t error;
    const char *filename;
    size_t offset;
    int docnum;
-   int fd;
    int i;
 
    /*
     * Print program usage if no arguments are provided.
     */
    if (argc == 1) {
-      fprintf(stderr, "usage: %s FILE...\n", argv[0]);
+      fprintf (stderr, "usage: %s FILE...\n", argv[0]);
       return 1;
    }
 
@@ -102,21 +61,13 @@ main (int   argc,
       docnum = 0;
 
       /*
-       * Open the filename provided in command line arguments.
-       */
-      errno = 0;
-      fd = bson_open(filename, O_RDONLY);
-      if (! (-1 != fd)) {
-         fprintf(stderr, "Failed to open %s: %s\n", filename, strerror(errno));
-         continue;
-      }
-
-      /*
        * Initialize a new reader for this file descriptor.
        */
-   reader = bson_reader_new_from_handle ((void *)&fd,
-                                         &_read_cb,
-                                         &_destroy_cb);
+      if (!(reader = bson_reader_new_from_file (filename, &error))) {
+         fprintf (stderr, "Failed to open \"%s\": %s\n",
+                  filename, error.message);
+         continue;
+      }
 
       /*
        * Convert each incoming document to JSON and print to stdout.
@@ -125,13 +76,12 @@ main (int   argc,
          docnum++;
          if (!bson_validate(b,
                             (BSON_VALIDATE_UTF8 |
-                             BSON_VALIDATE_DOLLAR_KEYS |
-                             BSON_VALIDATE_DOT_KEYS |
                              BSON_VALIDATE_UTF8_ALLOW_NULL),
                             &offset)) {
             fprintf(stderr,
                     "Document %u in \"%s\" is invalid at offset %u.\n",
                     docnum, filename, (int)offset);
+            bson_reader_destroy (reader);
             return 1;
          }
       }
