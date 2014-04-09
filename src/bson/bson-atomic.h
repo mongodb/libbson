5fd237b4f3a7e7a4ec8af73bc52650efefeb7e0d
Christian Hergert <christian.hergert@mongodb.com>
2014-04-08 17:17:55 -0700

    atomic: cleanup atomics handling and add fallbacks using a mutex.

--- src/bson/bson-atomic.h
+++ src/bson/bson-atomic.h
@@ -1,5 +1,5 @@
 /*
- * Copyright 2013 MongoDB, Inc.
+ * Copyright 2013-2014 MongoDB, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -26,30 +26,40 @@
 BSON_BEGIN_DECLS
 
 
-#if defined(__GNUC__)
-# if __GNUC__ >= 4 || (__GNUC__ == 4 && __GNUC_MINOR__ >= 1)
-#  define bson_atomic_int_add(p,v)   __sync_add_and_fetch(p,v)
-#  define bson_atomic_int64_add(p,v) __sync_add_and_fetch_8(p,v)
-#  define bson_memory_barrier()      __sync_synchronize()
-# elif defined(__sun)
-#  include <atomic.h>
-#  define bson_memory_barrier()      __asm__ volatile ("":::"memory")
-#  define bson_atomic_int_add(p,v)   atomic_add_int_nv(p,v)
-#  define bson_atomic_int64_add(p,v) atomic_add_64_nv(p,v)
-# else
-#  error "Unsupported GCC, missing atomics."
-# endif
+#if defined(__sun) && defined(__SVR4)
+# include <atomic.h>
+# define bson_atomic_int_add(p,v)   atomic_add_int_nv(p,v)
+# define bson_atomic_int64_add(p,v) atomic_add_64_nv(p,v)
 #elif defined(_WIN32)
 # define bson_atomic_int_add(p, v)   (InterlockedExchangeAdd((long int *)(p), v))
 # define bson_atomic_int64_add(p, v) (InterlockedExchangeAdd64(p, v))
-# define bson_memory_barrier         MemoryBarrier
-#elif defined(__sun)
-# include <atomic.h>
-# define bson_memory_barrier()       __machine_rw_barrier()
-# define bson_atomic_int_add(p,v)    atomic_add_int_nv(p,v)
-# define bson_atomic_int64_add(p,v)  atomic_add_64_nv(p,v)
+#elif defined(__GNUC__) && ((__GNUC__ > 4) || ((__GNUC__ == 4) && (__GNUC_MINOR__ >= 1)))
+# define bson_atomic_int_add(p,v)   __sync_add_and_fetch(p,v)
+# define bson_atomic_int64_add(p,v) __sync_add_and_fetch_8(p,v)
 #else
-# error "Unsupported Compiler, please add atomic's support."
+# warning "Unsupported Compiler/OS combination, please add support for atomics in bson-atomic.h. Using Mutex to emulate atomics."
+# define __BSON_NEED_ATOMICS 1
+  int     bson_atomic_int_add   (int *p, int n);
+  int64_t bson_atomic_int64_add (int64_t *p, int64_t n);
+#endif
+
+
+#if defined(__GNUC__)
+# if __GNUC__ >= 4 || (__GNUC__ == 4 && __GNUC_MINOR__ >= 1)
+#  define bson_memory_barrier() __sync_synchronize()
+# else
+#  warning "GCC Pre-4.1 discovered, using inline assembly for memory barrier."
+#  define bson_memory_barrier() __asm__ volatile ("":::"memory")
+# endif
+#elif defined(__SUNPRO_C)
+# include <mbarrier.h>
+# define bson_memory_barrier() __machine_rw_barrier()
+#elif defined(_WIN32)
+# define bson_memory_barrier() MemoryBarrier()
+#else
+# define __BSON_NEED_BARRIER 1
+# warning "Unknown compiler, using lock for compiler barrier."
+void bson_memory_barrier (void);
 #endif
 
 
