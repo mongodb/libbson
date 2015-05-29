9eb14d12c8a3495e0c99f2fb6238167f050df559
A. Jesse Jiryu Davis <jesse@mongodb.com>
2015-05-29 15:00:57 -0400

    CDRIVER-652 "%g" for floats in bson_as_json in 1.2.x

--- tests/test-json.c
+++ tests/test-json.c
@@ -122,10 +122,18 @@ test_bson_as_json_double (void)
 
    b = bson_new();
    assert(bson_append_double(b, "foo", -1, 123.456));
+   assert(bson_append_double(b, "bar", -1, 3));
+   assert(bson_append_double(b, "baz", -1, -1));
+   assert(bson_append_double(b, "quux", -1, 1.0001));
+   assert(bson_append_double(b, "huge", -1, 1e99));
    str = bson_as_json(b, &len);
-   assert(len >= 19);
-   assert(!strncmp("{ \"foo\" : 123.456", str, 17));
-   assert(!strcmp(" }", str + len - 2));
+   ASSERT_CMPSTR (str,
+                  "{"
+                  " \"foo\" : 123.456,"
+                  " \"bar\" : 3,"
+                  " \"baz\" : -1,"
+                  " \"quux\" : 1.0001,"
+                  " \"huge\" : 1e+99 }");
    bson_free(str);
    bson_destroy(b);
 }
