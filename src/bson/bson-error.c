307b436937c064c5d3e1a33729c5a582e1ccaae4
A. Jesse Jiryu Davis <jesse@mongodb.com>
2017-01-06 13:23:19 -0500

    CDRIVER-1982 fix ifdef for strerror_s with mingw

--- src/bson/bson-error.c
+++ src/bson/bson-error.c
@@ -105,12 +105,12 @@ bson_strerror_r (int err_code,  /* IN */
    static const char *unknown_msg = "Unknown error";
    char *ret = NULL;
 
-#if defined(__GNUC__) && defined(_GNU_SOURCE)
-   ret = strerror_r (err_code, buf, buflen);
-#elif defined(_WIN32)
+#if defined(_WIN32)
    if (strerror_s (buf, buflen, err_code) != 0) {
       ret = buf;
    }
+#elif defined(__GNUC__) && defined(_GNU_SOURCE)
+   ret = strerror_r (err_code, buf, buflen);
 #else /* XSI strerror_r */
    if (strerror_r (err_code, buf, buflen) == 0) {
       ret = buf;
