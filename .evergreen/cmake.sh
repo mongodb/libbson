db3b2824d8276941ea202bd53c450de309719173
A. Jesse Jiryu Davis <jesse@mongodb.com>
2017-02-18 10:57:56 -0500

    CDRIVER-1058 cmake package config

--- .evergreen/cmake.sh
+++ .evergreen/cmake.sh
@@ -9,8 +9,22 @@ else
    TAR=tar
 fi
 $TAR xf ../libbson.tar.gz -C . --strip-components=1
+
+SRCROOT=`pwd`
 mkdir cmake
 cd cmake
-/opt/cmake/bin/cmake -Wno-dev -Werror -Wdeprecated --warn-uninitialized --warn-unused-vars  ../.
+/opt/cmake/bin/cmake -Wno-dev -Werror -Wdeprecated --warn-uninitialized --warn-unused-vars -DCMAKE_INSTALL_PREFIX=`pwd` ..
 make
 make test
+make install
+
+# Test our CMake package config file
+cd $SRCROOT/examples/cmake/find_package
+/opt/cmake/bin/cmake -DCMAKE_PREFIX_PATH=$SRCROOT/cmake/lib/cmake .
+make
+LD_LIBRARY_PATH=$SRCROOT/cmake ./hello_bson
+
+cd $SRCROOT/examples/cmake/FindPkgConfig
+PKG_CONFIG_PATH=$SRCROOT/cmake /opt/cmake/bin/cmake .
+make
+LD_LIBRARY_PATH=$SRCROOT/cmake ./hello_bson
---
+++ build/cmake/BSONPackage.cmake
@@ -0,0 +1,26 @@
+set (PACKAGE_INCLUDE_INSTALL_DIRS ${BSON_HEADER_INSTALL_DIR})
+set (PACKAGE_LIBRARY_INSTALL_DIRS lib)
+set (PACKAGE_LIBRARIES bson-1.0)
+
+include (CMakePackageConfigHelpers)
+
+configure_package_config_file (
+   build/cmake/libbson-1.0-config.cmake.in
+   ${CMAKE_CURRENT_BINARY_DIR}/libbson-1.0-config.cmake
+   INSTALL_DESTINATION lib/cmake/libbson-1.0
+   PATH_VARS PACKAGE_INCLUDE_INSTALL_DIRS PACKAGE_LIBRARY_INSTALL_DIRS
+)
+
+write_basic_package_version_file (
+   ${CMAKE_CURRENT_BINARY_DIR}/libbson-1.0-config-version.cmake
+   VERSION ${BSON_VERSION}
+   COMPATIBILITY AnyNewerVersion
+)
+
+install (
+   FILES
+      ${CMAKE_CURRENT_BINARY_DIR}/libbson-1.0-config.cmake
+      ${CMAKE_CURRENT_BINARY_DIR}/libbson-1.0-config-version.cmake
+   DESTINATION
+      lib/cmake/libbson-1.0
+)
