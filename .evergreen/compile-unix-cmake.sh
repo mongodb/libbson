342189792375db3253d4f84c8674b58bfbdc751e
A. Jesse Jiryu Davis <jesse@mongodb.com>
2017-02-24 06:37:45 -0500

    CDRIVER-1058 update cmake evergreen script

--- .evergreen/compile-unix-cmake.sh
+++ .evergreen/compile-unix-cmake.sh
@@ -2,29 +2,38 @@
 set -o xtrace   # Write all commands first to stderr
 set -o errexit  # Exit the script with error if any of the commands fail
 
+CMAKE=/opt/cmake/bin/cmake
 
 if command -v gtar 2>/dev/null; then
    TAR=gtar
 else
    TAR=tar
 fi
-$TAR xf ../libbson.tar.gz -C . --strip-components=1
 
 SRCROOT=`pwd`
-mkdir cmake
-cd cmake
-/opt/cmake/bin/cmake -Wno-dev -Werror -Wdeprecated --warn-uninitialized --warn-unused-vars -DCMAKE_INSTALL_PREFIX=`pwd` ..
+
+BUILD_DIR=$(pwd)/cbuild
+rm -rf $BUILD_DIR
+mkdir $BUILD_DIR
+
+INSTALL_DIR=$(pwd)/install-dir
+rm -rf $INSTALL_DIR
+mkdir -p $INSTALL_DIR
+
+cd $BUILD_DIR
+$TAR xf ../../libbson.tar.gz -C . --strip-components=1
+$CMAKE -Wno-dev -Werror -Wdeprecated --warn-uninitialized --warn-unused-vars -DCMAKE_INSTALL_PREFIX=$INSTALL_DIR .
 make
 make test
 make install
 
 # Test our CMake package config file
 cd $SRCROOT/examples/cmake/find_package
-/opt/cmake/bin/cmake -DCMAKE_PREFIX_PATH=$SRCROOT/cmake/lib/cmake .
+$CMAKE -DCMAKE_PREFIX_PATH=$INSTALL_DIR/lib/cmake .
 make
-LD_LIBRARY_PATH=$SRCROOT/cmake ./hello_bson
+LD_LIBRARY_PATH=$INSTALL_DIR/lib ./hello_bson
 
 cd $SRCROOT/examples/cmake/FindPkgConfig
-PKG_CONFIG_PATH=$SRCROOT/cmake /opt/cmake/bin/cmake .
+PKG_CONFIG_PATH=$INSTALL_DIR/lib/pkgconfig /opt/cmake/bin/cmake .
 make
-LD_LIBRARY_PATH=$SRCROOT/cmake ./hello_bson
+LD_LIBRARY_PATH=$INSTALL_DIR/lib ./hello_bson
