94a9eb14d99056d1e00845dc516fb9d43709a412
Hannes Magnusson <bjori@php.net>
2017-06-01 11:57:26 -0700

    Fix uploading the artifacts

--- .evergreen/compile.sh
+++ .evergreen/compile.sh
@@ -4,6 +4,10 @@ set -o errexit  # Exit the script with error if any of the commands fail
 
 OS=$(uname -s | tr '[:upper:]' '[:lower:]')
 
+echo '{ "results": [ { "status": "pass", "test_file": "Create Release Archive",' > test-results.json
+start=$(date +%s)
+echo '"start": ' $start ', ' >> test-results.json
+
 case "$OS" in
    cygwin*)
       sh ./.evergreen/compile-windows.sh
@@ -13,4 +17,9 @@ case "$OS" in
       sh ./.evergreen/compile-unix.sh
    ;;
 esac
+
+end=$(date +%s)
+echo '"end": ' $end ', ' >> test-results.json
+sum=$(expr $end - $start || true)
+echo '"elapsed": ' $sum ' } ] }' >> test-results.json
 
